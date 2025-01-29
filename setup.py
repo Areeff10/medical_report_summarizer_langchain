d529eb52a817da8cf19e0c8fcc0ed6c7d0df1f2d
Areef Khan <128778611+Areeff10@users.noreply.github.com>
2025-01-29 08:12:51 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Langchain',
+    version= '2.0.0',
+    author= 'Areef Khan',
+    author_email= 'akhan4@gitam.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
